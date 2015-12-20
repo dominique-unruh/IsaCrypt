ec410a848d8a158647e19c346664ba553a51d855
Dominique Unruh <unruh@ut.ee>
2015-12-20 21:25:44 +0200

    lemma denotation_footprint_def2
    Split Scratch.thy

--- Scratch.thy
+++ Scratch.thy
@@ -329,8 +329,52 @@ lemma program_readonly_write_vars: "program_readonly (- set(write_vars p)) p"
   unfolding program_readonly_def program_untyped_readonly_def write_vars_def denotation_def 
   by assumption
 
+(* lemma denotation_footprint_def2: "denotation_footprint X d = ((\<forall>m m' z. 
+  (\<forall>x\<in>-X. Rep_memory m x = Rep_memory m' x) \<longrightarrow> Rep_distr (d m) m' = Rep_distr (d (memory_combine X m z)) (memory_combine X m' z))
+  \<and> (\<forall>m m' z. 
+  ~ (\<forall>x\<in>-X. Rep_memory m x = Rep_memory m' x) \<longrightarrow> Rep_distr (d m) m' = 0))"
+  unfolding denotation_footprint_def apply auto *)
+
+lemma program_untyped_footprint_vars: "program_untyped_footprint (set(vars_untyped p)) p"
+proof -
+  fix q
+  have "\<And>R. set (vars_untyped p) \<subseteq> R \<Longrightarrow> program_untyped_footprint R p"
+    and "\<And>R. set (vars_proc_untyped q) \<subseteq> R \<Longrightarrow> True" 
+  proof (induct p and q)
+  case Assign thus ?case by auto
+  next case Sample thus ?case by auto
+  next case Skip 
+    have aux: "\<And>f g x. f = g \<Longrightarrow> f x = g x" by simp
+    have "\<And>m m'. Rep_memory (memory_combine R default m) = Rep_memory (memory_combine R default m') \<Longrightarrow>
+       Rep_memory m' \<noteq> Rep_memory m \<Longrightarrow> (\<And>z. Rep_memory (memory_combine R m' z) \<noteq> Rep_memory (memory_combine R m z))"
+    proof -
+      fix m m' z
+      assume "Rep_memory m' \<noteq> Rep_memory m" then obtain x where x: "Rep_memory m' x \<noteq> Rep_memory m x" by auto
+      assume "Rep_memory (memory_combine R default m) = Rep_memory (memory_combine R default m')"
+      with x have "x \<in> R" apply (cases "x\<in>R") apply auto apply (drule_tac aux[where x=x]) by auto
+      with x show "Rep_memory (memory_combine R m' z) \<noteq> Rep_memory (memory_combine R m z)" 
+        apply auto apply (drule_tac aux[where x=x]) by auto
+    qed
+    thus ?case
+      unfolding program_untyped_footprint_def denotation_footprint_def apply simp
+      apply (subst Rep_memory_inject[symmetric])+ by simp
+  next case While thus ?case by auto
+  next case IfTE thus ?case by auto
+  next case Seq thus ?case by auto
+  next case CallProc thus ?case by auto
+  next case Proc thus ?case by auto
+  next case ProcAppl thus ?case by auto
+  next case ProcPair thus ?case by auto
+  next case ProcRef thus ?case by auto
+  next case ProcAbs thus ?case by auto
+  next case ProcUnpair thus ?case by auto
+  qed
+  thus ?thesis by auto
+qed
+
 lemma program_footprint_vars: "program_footprint (set(vars p)) p"
-SORRY
+  using program_untyped_footprint_vars
+  unfolding program_footprint_def program_untyped_footprint_def vars_def denotation_def.
 
 
 lemma seq_swap2:
@@ -446,99 +490,5 @@ by (fact assms)
 
 
 
-
-
-ML {*
-type module = {
-  name : string list
-}
-
-fun module_name ({name=name, ...} : module) = String.concatWith "." (rev name)
-*}
-
-
-ML {*
-structure ModuleData = Generic_Data
-  (type T = module list
-   val empty = []
-   val extend = I
-   fun merge (_,_) = error ("ModuleData.merge"))
-*}
-                                             
-ML {*
-
-fun begin_module1 (name:string) lthy : local_theory =
-  let val _ = @{print} name
-      val _ = @{print} lthy
-      val module_stack = ModuleData.get (Context.Proof lthy)
-      val full_name = case module_stack of [] => [name] | {name=n,...}::_ => name::n
-      val new_module = {name=full_name}
-      val lthy = ModuleData.map (fn d => new_module::d) (Context.Proof lthy) |> Context.proof_of
-  in
-  lthy
-  end
-
-fun begin_module (name:string) =
-  let fun begin stack = 
-        let val full_name = case stack of [] => [name] | {name=n,...}::_ => name::n
-            val new_module = {name=full_name}
-        in new_module::stack end
-  in
-  Local_Theory.declaration {pervasive=true, syntax=false}
-  (fn _ => ModuleData.map begin)
-  end
-
-fun end_module lthy =
-  let val stack = ModuleData.get (Context.Proof lthy)
-      val _ = if null stack then error "No matching module command" else ()
-      val _ = writeln ("Closing module "^module_name (hd stack))
-  in
-  Local_Theory.declaration {pervasive=true, syntax=false} (fn _ => ModuleData.map tl) lthy
-  end
-
-val _ =
-  Outer_Syntax.command @{command_keyword module} "Defines a new module"
-    (Parse.name --| Parse.begin
-      >> (fn name => Toplevel.local_theory NONE NONE (begin_module name)))
-val _ =
-  Outer_Syntax.command @{command_keyword end_module} "Finished a module definition"
-    (Scan.succeed (Toplevel.local_theory NONE NONE end_module))
-*}
-
-ML {*
-fun current_module ctx = 
-  case ModuleData.get (Context.Proof ctx) of [] => NONE
-                           | m::_ => SOME m
-fun current_module_name ctx =
-  case current_module ctx of NONE => [] | SOME m => #name m
-
-fun qualify_in_module ctx bind =
-  fold (Binding.qualify true) (current_module_name ctx) bind
-*}
-
-module hello begin
-module hey begin
-
-ML {* qualify_in_module @{context} @{binding beep} *}
-
-local_setup {* fn lthy => 
-  let val (_,lthy) = Local_Theory.define ((qualify_in_module lthy @{binding bla},NoSyn),((@{binding bla_def},[]),@{term "1::int"})) lthy
-  in
-  lthy
-  end
-*}
-
-thm bla_def
-
-ML {* ModuleData.get (Context.Proof @{context}) |> hd |> module_name *}
-
-end_module
-end_module
-
-
-thm bla_def
-
-module_type MT =
-  proc1 :: "(unit,unit) procedure"
 
 end
